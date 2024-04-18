cf9ead519a1a336ff69ce941fd8abd2d2749a947
msp3021 <131592979+msp3021@users.noreply.github.com>
2024-04-18 14:12:05 +0200

    homework 1

--- js/task-1.js
+++ js/task-1.js
@@ -3,6 +3,7 @@ function makeTransaction(quantity, pricePerDroid){
     const totalPrize = quantity * pricePerDroid;
     console.log(`"You ordered ${quantity} droids worth ${totalPrize} credits!"`);
 
-};
-//console.log(makeTransaction(5, 3000));
-//console.log(makeTransaction(3, 1000));
+}
+console.log(makeTransaction(5, 3000));
+console.log(makeTransaction(3, 1000));
+console.log(makeTransaction(10, 500));
